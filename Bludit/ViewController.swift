8a3e9a28240dcdeffc5879c04489bc901a8b009c
Viktor Gordienko (intern) <battlethem@yahoo.com>
2020-04-02 11:16:31 +0300

    Initial Commit

---
+++ Bludit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bludit
+//
+//  Created by Viktor Gordienko on 4/2/20.
+//  Copyright © 2020 Viktor Gordienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BluditTests/BluditTests.swift
@@ -0,0 +1,34 @@
+//
+//  BluditTests.swift
+//  BluditTests
+//
+//  Created by Viktor Gordienko on 4/2/20.
+//  Copyright © 2020 Viktor Gordienko. All rights reserved.
+//
+
+import XCTest
+@testable import Bludit
+
+class BluditTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
